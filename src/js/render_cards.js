a9741d2ab512d0e62b146539f377dcaaee72f14c
Stanislav <Stas.zaliiski@gmail.com>
2023-04-10 20:13:50 +0200

    main

--- src/js/render_cards.js
+++ src/js/render_cards.js
@@ -1,38 +1,39 @@
 import ApiServices from './services/Api_services';
-const BASE_URL_POSTER = 'https://image.tmdb.org/t/p/w500/'
+const BASE_URL_POSTER = 'https://image.tmdb.org/t/p/w500/';
 const colectionUl = document.querySelector('.collection');
 const apiServices = new ApiServices();
 
 searchStart();
 
-async function searchStart () {
-    // btnPagination.style.display = 'none';
-            
-    try {
-        const {data} = await apiServices.getTrendMovies();
-        console.log(data);
-            // renderMarkur(data.results);
-        }
-        catch (error) {
-            console.log(error.message);
-            return;        
-        };
-        // btnPagination.style.display = 'flex';
-        // btnPaginationBack.style.display = 'none';
-};
+async function searchStart() {
+  // btnPagination.style.display = 'none';
 
+  try {
+    const { data } = await apiServices.getTrendMovies();
+    console.log(data);
+    renderMarkur(data.results);
+  } catch (error) {
+    console.log(error.message);
+    return;
+  }
+  btnPagination.style.display = 'flex';
+  btnPaginationBack.style.display = 'none';
+}
 
-// function renderMarkur (data) {
-//     const markup = data.map(({ id, title, poster_path }) =>
-//       `  <li class="card">
-//           <a>
-//             <img src="${BASE_URL_POSTER}${poster_path}" class="card__img" />
-//           </a>
+function renderMarkur(data) {
+  const markup = data
+    .map(
+      ({ id, title, poster_path }) =>
+        `  <li class="card">
+          <a>
+            <img src="${BASE_URL_POSTER}${poster_path}" class="card__img" />
+          </a>
 
-//           <div class="card__title">${title}</div>
-//           <div class="card__info">жанри та рік</div>
-//         </li>
-//    `
-//     ).join('');
-//     colectionUl.insertAdjacentHTML('beforeend', markup);
-// }
+          <div class="card__title">${title}</div>
+          <div class="card__info">жанри та рік</div>
+        </li>
+   `
+    )
+    .join('');
+  colectionUl.insertAdjacentHTML('beforeend', markup);
+}
