42ba9686f46fec4f0654eee2a342c9849a27899f
Stanislav <Stas.zaliiski@gmail.com>
2023-04-10 09:31:52 +0200

    main

--- src/modal.js
+++ src/modal.js
@@ -1,56 +1,61 @@
-
 // import ApiServices from './js/services/api_services';
 
-      const refs = {
-        openModal: document.querySelector("button"),
-        closeModalBtn: document.querySelector("[data-modal-about-close]"),
-        modal: document.querySelector("[data-modal-about]"),
-      };
-    
-      refs.openModal.addEventListener("click", openModalHome);
-      refs.closeModalBtn.addEventListener("click", closeModal);
-    
-      function closeModal() {
-        refs.modal.classList.add("is-hidden");
-      }
-      
-      function openModalElem() {
-        refs.modal.classList.remove("is-hidden");
-      }
+const refs = {
+  openModal: document.querySelector('button'),
+  closeModalBtn: document.querySelector('[data-modal-about-close]'),
+  modal: document.querySelector('[data-modal-about]'),
+};
 
-  const apiServices = new ApiServices();
+refs.openModal.addEventListener('click', openModalHome);
+refs.closeModalBtn.addEventListener('click', closeModal);
+
+function closeModal() {
+  refs.modal.classList.add('is-hidden');
+}
+
+function openModalElem() {
+  refs.modal.classList.remove('is-hidden');
+}
+
+const apiServices = new ApiServices();
 
 // на який елемент вішаємо слухача для kліку для відкриття модалки? - після рендеру головної сторінки.
-    // яку властивість брати за ключову для пошуку?
+// яку властивість брати за ключову для пошуку?
 
-  // модалка для трендового фільма
-  function openModalHome (e) {
-    console.log(`да`);
-    openModalElem();
-    e.preventDefault();
- 
-    apiServices.getTrendMovies().then(movie => {
+// модалка для трендового фільма
+function openModalHome(e) {
+  console.log(`да`);
+  openModalElem();
+  e.preventDefault();
 
-  //       let currentID = e.currentTarget.elements.searchQuery.value;
-  //  const movie =  trendMovies.find((trendMovie => trendMovie.id === currentID));
-       refs.modal.insertAdjacentHTML("beforeend", movieCard(movie));
-        
-   })
-  }
+  apiServices.getTrendMovies().then(movie => {
+    //       let currentID = e.currentTarget.elements.searchQuery.value;
+    //  const movie =  trendMovies.find((trendMovie => trendMovie.id === currentID));
+    refs.modal.insertAdjacentHTML('beforeend', movieCard(movie));
+  });
+}
 // модалка для пошуку за ключовим словом
-  //  function openModalKey (e) {
-  //   e.preventDefault();
-  //       let currentID = e.currentTarget.elements.searchQuery.value;
-  //  const movie =  searchMovies.find((searchMovie => searchMovie.id === currentID));
-  //      refs.modal.insertAdjacentHTML("beforeend", movieCard(movie));
-        
-  //  }
+//  function openModalKey (e) {
+//   e.preventDefault();
+//       let currentID = e.currentTarget.elements.searchQuery.value;
+//  const movie =  searchMovies.find((searchMovie => searchMovie.id === currentID));
+//      refs.modal.insertAdjacentHTML("beforeend", movieCard(movie));
 
+//  }
 
-  function movieCard (movie) {
-    return movie
-      .map(({original_title, backdrop_path, vote_average, vote_count, popularity, genre_ids, overview }) => {
-       return `<div class="movie__modal is-hidden" data-modal-about>
+function movieCard(movie) {
+  return movie
+    .map(
+      ({
+        original_title,
+        backdrop_path,
+        vote_average,
+        vote_count,
+        popularity,
+        genre_ids,
+        overview,
+      }) => {
+        return `<div class="movie__modal is-hidden" data-modal-about>
        <button class="movie__close"  data-modal-about-close>
           <svg class='icon-close' width="14" height="14" >
                     <use href="./images/sprite.svg#icon-Vectorclose"></use>
@@ -78,7 +83,7 @@
             <span >${popularity}</span></p>
          <p class="info-item--title"><span >${original_title}</span></p>
          <p class="info-item">
-             <span > ${Object.values(genre_ids).join(", ")}</span></p>
+             <span > ${Object.values(genre_ids).join(', ')}</span></p>
       </div>
    </div>
          <p class="movie__about--modal"><b>ABOUT</b></p>
@@ -96,10 +101,9 @@
           <button class="movie__queue"  data-modal-queue data-modal-delete><span>add to queue</span></button>  
         </div> 
 </div>`;
-}).join('');
+      }
+    )
+    .join('');
 }
 
-// import Player  from '@vimeo/player';     
-
-
-
+// import Player  from '@vimeo/player';
