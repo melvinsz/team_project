eea00548ee979be165fae1ccda74a81ab0320c7b
Kolvad <vskolesnik@gmail.com>
2023-04-12 01:47:42 +0300

    fixed library html js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,3 @@ import './js/modal';
 import './js/render_cards';
 import './js/add_to_watched';
 import './js/pagination_main';
-import './js/wathchedbutton';
--- src/library.js
+++ src/library.js
@@ -1,2 +1,3 @@
 import './js/pagination_2';
 import './js/add_to_watched';
+import './js/wathched_button';
