a61d0a33d53a547918755acb72d1a9b64cd21799
TetianaAsadova <114417194+TetianaAsadova@users.noreply.github.com>
2023-04-11 11:51:52 +0300

    sum3

--- src/js/addqueue.js
+++ src/js/addqueue.js
@@ -1,6 +1,4 @@
-import '../sass/_addqueue.scss';
 
-const BASE_URL = 'https://api.themoviedb.org/3/movie/550?api_key=';
 const pageNumber = 1;
 
 const refs = {
@@ -58,3 +56,47 @@ async function queueMovies(e) {
 //     .join('');
 //     refs.container.innerHTML = markup;      
 }
+
+
+const STORAGE_KEY = 'watched-films';
+ localStorage.setItem(STORAGE_KEY, '[]');
+ const filmListFromStorage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+// export function addToWatched(data) {
+//   const filmToAdd = data;
+//   console.log(filmToAdd);
+
+//   const addToWatchedRef = document.querySelector('[data-modal-add]');
+//   const parsedWatchedFilms = JSON.parse(localStorage.getItem(STORAGE_KEY));
+//   console.log(watchedFilms);
+
+//   //(data.id === parsedWatchedFilms.find(element => element.id))
+//   //(listFilmToWatched.some(film=> film.id==data.id))
+//   //варіант з прикладу if (parsedWatchedFilms.some(element => element.id === filmToAdd.id))
+
+//   if (filmToAdd.id === parsedWatchedFilms.some(element => element.id)) {
+//     addToWatchedRef.textContent = `Remove from watched`;
+//   }
+
+//   addToWatchedRef.addEventListener('click', onAddToWatched);
+
+//   function onAddToWatched(e) {
+//     const filmToAdd = data;
+
+//     console.log(`Фільм додається до watched`);
+//     console.log(localStorage.getItem(STORAGE_KEY));
+
+//     if (filmToAdd.id === parsedWatchedFilms.some(element => element.id)) {
+//       removeFromWatched(filmToAdd.id);
+//       addToWatchedRef.textContent = `Add to watched`;
+//     } else {
+//       parsedWatchedFilms.push(filmToAdd);
+//       localStorage.setItem(STORAGE_KEY, JSON.stringify(parsedWatchedFilms));
+//       addToWatchedRef.textContent = `Remove from watched`;
+//     }
+//   }
+// }
+
+// function removeFromWatched(id) {
+//   console.log(`removing movie with id ${id}`);
+// }
