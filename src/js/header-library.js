14ae0024ce8c0fa3d16ae3ffd7fabe0df7e892cb
Stanislav <Stas.zaliiski@gmail.com>
2023-04-13 21:18:35 +0200

    main

--- src/js/header-library.js
+++ src/js/header-library.js
@@ -5,30 +5,25 @@ import localStore from './services/local_storage.js';
 // import getGenres from './services/connect_genres.js';
 // import loader from './loader';
 
-
 export const refs = {
-    btnWatched: document.querySelector('#btnWatched'),
-    // btnQueued: document.querySelector('#btnQueued'),
-  };
+  btnWatched: document.querySelector('#btnWatched'),
+  // btnQueued: document.querySelector('#btnQueued'),
+};
 
-  console.log(refs.btnWatched);
-  
+console.log(refs.btnWatched);
 
 // refs.btnQueued.addEventListener('click', clickOnBtnQueuedHandler);
 // refs.btnWatched.addEventListener('click', clickOnBtnWatchedHandler);
 
-// const queueMovies = localStore.load('queue-movies');
+const queueMovies = localStore.load('queue-movies');
 console.log(queueMovies);
 
-
 function clickOnBtnQueuedHandler() {
-    refs.btnWatched.classList.remove('active-btn');
-    refs.btnQueued.classList.add('active-btn');
-    
-  }
-  
-  function clickOnBtnWatchedHandler() {
-    refs.btnQueued.classList.remove('active-btn');
-    refs.btnWatched.classList.add('active-btn');
-    
-  }
+  refs.btnWatched.classList.remove('active-btn');
+  refs.btnQueued.classList.add('active-btn');
+}
+
+function clickOnBtnWatchedHandler() {
+  refs.btnQueued.classList.remove('active-btn');
+  refs.btnWatched.classList.add('active-btn');
+}
